a4c52eb36cabe060876925ce16315d0a2d0f80e3
tanktoptony <anthony.jarina@gmail.com>
2024-05-21 13:00:09 -0500

    vue implemented and vite browser cleaned up

--- back_end/backend/settings.py
+++ back_end/backend/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    "graphene_django",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ("http://localhost:5173",)
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -70,7 +75,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
+GRAPHENE = {
+    "SCHEMA": "blog.schema.schema",
+}
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
